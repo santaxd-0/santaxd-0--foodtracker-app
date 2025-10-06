8bb977c10d992437333d5ce2ee9669fc5879cac9
Alexander Zhuravel <alexzhuravel007@gmail.com>
2025-10-07 00:38:36 +0300

    Added button for login page in desktop_ios

--- desktop_ios/desktop_ios/ContentView.swift
+++ desktop_ios/desktop_ios/ContentView.swift
@@ -11,29 +11,49 @@ struct ContentView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     
+    func logIn(){
+        print("Login user")
+    }
+    
     var body: some View {
         HStack{
-            VStack(spacing: 20) {
+            VStack(spacing: 30) {
                 Text("Log in")
                     .font(.largeTitle)
                     .fontWeight(.bold)
-                VStack(spacing: 15) {
-                    VStack(alignment: .leading, spacing: 10) {
+                VStack(spacing: 20) {
+                    VStack(alignment: .leading, spacing: 15) {
                         Text("Enter your email: ")
+                            .font(.system(size: 16))
                         TextField("Enter your email", text: $email)
                             .lineLimit(3, reservesSpace: true)
                             .textFieldStyle(.roundedBorder)
                             .frame(width: 300)
+                            .font(.system(size: 16))
                     }
-                    VStack(alignment: .leading, spacing: 10) {
+                    VStack(alignment: .leading, spacing: 15) {
                         Text("Enter your password: ")
+                            .font(.system(size: 16))
                         SecureField("Enter your password", text: $password)
                             .lineLimit(3, reservesSpace: true)
                             .textFieldStyle(.roundedBorder)
                             .frame(width: 300)
+                            .font(.system(size: 16))
                     }
+                    Button(action: logIn){
+                        Label("Sign In", systemImage: "arrow.up")
+                            .frame(width: 290, height: 30)
+                            .font(.system(size: 16))
+                            .bold()
+                    }
+                    .background(
+                        Color(red: 0.01, green: 0.5, blue: 0.32)
+                    )
+                    .cornerRadius(10)
                 }
-            }.padding()
+            }
+            .padding()
+            
             Image("loginImage")
                     .resizable()
                     .scaledToFit()
