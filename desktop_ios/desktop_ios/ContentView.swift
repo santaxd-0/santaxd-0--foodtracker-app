417c1bdf4aae6cabe4cc5455073fe0937418e0fd
Alexander Zhuravel <alexzhuravel007@gmail.com>
2025-10-06 23:55:13 +0300

    Created simple login screen for desktop ios app

--- desktop_ios/desktop_ios/ContentView.swift
+++ desktop_ios/desktop_ios/ContentView.swift
@@ -9,17 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var email: String = ""
+    @State private var password: String = ""
     
     var body: some View {
-        VStack {
-            Text("Log in")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            VStack {
-                Text("Hello!!!")
-                TextField("Enter your email", text: $email)
-            }.padding(10)
+        HStack{
+            VStack(spacing: 20) {
+                Text("Log in")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                VStack(spacing: 15) {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Enter your email: ")
+                        TextField("Enter your email", text: $email)
+                            .lineLimit(3, reservesSpace: true)
+                            .textFieldStyle(.roundedBorder)
+                            .frame(width: 300)
+                    }
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Enter your password: ")
+                        SecureField("Enter your password", text: $password)
+                            .lineLimit(3, reservesSpace: true)
+                            .textFieldStyle(.roundedBorder)
+                            .frame(width: 300)
+                    }
+                }
+            }.padding()
+            Image("loginImage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400)
         }
-        .padding()
     }
 }
